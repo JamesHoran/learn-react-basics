decced55cb557a1ab340d57da7e291dcb054e419
James Horan <jbolton128@gmail.com>
2025-10-30 09:03:35 -0500

    feat: implemented conditional rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { ListItem } from "./comps/ListItem";
 import List from "./comps/List";
+import { useState } from "react";
 
 export default function Home() {
   const items: Array<string> = ["item 1", "item 2"];
+  const [showList, setShowList] = useState<boolean>(true);
+
   return (
     <>
-      <List type={"unordered"}>
-        {items.map((i) => (
-          <ListItem key={i} item={i} />
-        ))}
-      </List>
+      <button onClick={() => setShowList(!showList)}>Show List</button>
+      {showList && (
+        <List type={"unordered"}>
+          {items.map((i) => (
+            <ListItem key={i} item={i} />
+          ))}
+        </List>
+      )}
     </>
   );
 }
